aa9adb9a813f2fbe69572343e92ce8b993b89892
Alexander Asanov <aleksandro.eterverda@gmail.com>
2015-06-29 12:32:12 +0300

    TimeChangedReceiver renamed to SNTPResetCacheReceiver

--- sntp-android/src/main/java/io/github/eterverda/sntp/android/SNTPResetCacheReceiver.java
+++ sntp-android/src/main/java/io/github/eterverda/sntp/android/SNTPResetCacheReceiver.java
@@ -8,17 +8,15 @@ import android.support.annotation.NonNull;
 import io.github.eterverda.sntp.SNTP;
 import io.github.eterverda.sntp.cache.SNTPCache;
 
-public final class TimeChangedReceiver extends BroadcastReceiver {
+public final class SNTPResetCacheReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(@NonNull Context context, @NonNull Intent intent) {
         final String action = intent.getAction();
-        if (!Intent.ACTION_TIME_CHANGED.equals(action)) {
-            return;
+        if (Intent.ACTION_TIME_CHANGED.equals(action)) {
+            final SNTPCache cache = SNTP.getCache();
+            if (cache != null) {
+                cache.put(null);
+            }
         }
-        final SNTPCache cache = SNTP.getCache();
-        if (cache == null) {
-            return;
-        }
-        cache.put(null);
     }
 }
